d92d2422ee7bdb8bbab95301f1f1ab41b8c1a618
Ian <holmesian17@gmail.com>
2017-07-04 13:13:47 -0500

    Update job-scrape.py

--- job-scrape.py
+++ job-scrape.py
@@ -7,30 +7,6 @@ import smtplib
 import bs4
 import pprint
 
-
-res = requests.get('https://multcolib.org/about/jobs-library') # Multnomah County
-res.raise_for_status()
-MCLsoup = bs4.BeautifulSoup(res.text, "lxml")
-newMCL = MCLsoup.select('#content')
-
-# opening and reading the old html doc as a string
-oldMCL = open('/home/ian/libsitehtml/oldMCL')
-oldMCL = oldMCL.read()
-
-#this successfully changes the old document to the new website
-if str(newMCL) != str(oldMCL):
-    #saves the page as a file to the HD if there are changes
-    file = open('/home/ian/libsitehtml/oldMCL', 'w') 
-    file.write(str(newMCL))
-    file.close()
-    #emails me if there are changes
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('noreply.job.updates@gmail.com', 'zxcvbnm1029384756')
-    server.sendmail('noreply.job.updates@gmail.com', 'holmesian17@gmail.com', 'Subject: MCL jobs page has changed\n' '\n Here is a link: https://multcolib.org/about/jobs-library')
-    server.quit()
-
 res = requests.get('https://libwww.freelibrary.org/jobs/JobList.cfm') # Philadelphia Free Library
 res.raise_for_status()
 PFLsoup = bs4.BeautifulSoup(res.text, "lxml")
